98e0ee21ef3edbe8c72b0e66f061380c264d184c
Joshua Daniel <joshua_dup@hotmail.com>
2025-11-10 21:12:39 -0500

    I create data file and global styles in index

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
+// Punto de entrada de la aplicación React
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
